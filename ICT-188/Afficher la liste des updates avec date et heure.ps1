910314ef8450491e774cfb7805e319dece37d1ed
Pascal KOTTE <GitHub.com@Kotte.net>
2024-02-15 13:28:16 +0100

    Update Afficher la liste des updates avec date et heure.ps1

--- ICT-188/Afficher la liste des updates avec date et heure.ps1
+++ ICT-188/Afficher la liste des updates avec date et heure.ps1
@@ -1,4 +1,6 @@
-﻿$Session = New-Object -ComObject "Microsoft.Update.Session"
+# Ce script affiche
+$Session = New-Object -ComObject "Microsoft.Update.Session"
 $Searcher = $Session.CreateUpdateSearcher()
 $historyCount = $Searcher.GetTotalHistoryCount()
 $Searcher.QueryHistory(0, $historyCount) | Select-Object Title, Date, @{name="Operation"; expression={switch($_.operation){ 1 {"Installation"}; 2 {"Uninstallation"}; 3 {"Other"}}}}
+# NB: Pour activer l'autorisation de script sur un PC Windows, il faut exécuter: Set-ExecutionPolicy Unrestricted
